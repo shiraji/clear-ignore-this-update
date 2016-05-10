df1ad799af168de4269ad6c4eb68c4e58ed7d96d
shiraji <isogai.shiraji@gmail.com>
2016-05-10 16:11:28 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/shiraji/clearignorethisupdate/ClearIgnoreThisUpdate.kt
@@ -0,0 +1,21 @@
+package com.github.shiraji.clearignorethisupdate
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ex.ApplicationEx
+import com.intellij.openapi.updateSettings.impl.UpdateSettings
+
+class ClearIgnoreThisUpdate : AnAction() {
+    override fun actionPerformed(e: AnActionEvent?) {
+        UpdateSettings.getInstance().ignoredBuildNumbers.clear()
+        val app = ApplicationManager.getApplication() as ApplicationEx
+        app.restart(true)
+    }
+
+    override fun update(e: AnActionEvent?) {
+        e ?: return
+        super.update(e)
+        e.presentation.isEnabledAndVisible = UpdateSettings.getInstance().ignoredBuildNumbers.size > 0
+    }
+}
